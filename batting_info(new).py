5b7dee33146891b9ae66cbeed86da02bfd15220e
김경욱 - Kyengwook, Kim <93772960+kyengwook@users.noreply.github.com>
2025-07-23 11:46:29 +0900

    Update batting_info(new).py

--- batting_info(new).py
+++ batting_info(new).py
@@ -27,24 +27,24 @@ def load_data_from_drive():
     df = df.set_index('game_date').sort_index()
     return df
 
-@st.cache_data
-def load_batter_id():
-    batter_ID = pd.read_excel('Batter_ID(2025).xlsx')
-    return batter_ID
-
 #@st.cache_data
-#def load_pitcher_id():
-    #pitcher_ID = pd.read_excel('Pitcher_ID(2025).xlsx')
-    #return pitcher_ID
+#def load_batter_id():
+    #batter_ID = pd.read_excel('Batter_ID(2025).xlsx')
+    #return batter_ID
+
+@st.cache_data
+def load_pitcher_id():
+    pitcher_ID = pd.read_excel('Pitcher_ID(2025).xlsx')
+    return pitcher_ID
 
 # -----------------------------
 # 데이터 불러오기
 # -----------------------------
 df = load_data_from_drive()
-batter_ID = load_batter_id()
-#pitcher_ID = load_pitcher_id()
+#batter_ID = load_batter_id()
+pitcher_ID = load_pitcher_id()
 
-df = pd.merge(df, batter_ID, on='batter', how='left')
+#df = pd.merge(df, batter_ID, on='batter', how='left')
 
 if df.empty:
     st.error("❌ 데이터셋이 비어있습니다. Google Drive 파일 ID나 파일 내용을 확인하세요.")
